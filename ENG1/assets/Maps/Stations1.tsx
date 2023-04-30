d91bab4bc7ae56dea075bd42cb0f38417934bb44
IvanNdahiro <in597@york.ac.uk>
2023-04-30 17:27:06 +0100

    added tsx file

---
+++ ENG1/assets/Maps/Stations1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Stations" tilewidth="64" tileheight="32" tilecount="200" columns="10">
+ <image source="Stations.png" width="640" height="640"/>
+</tileset>
